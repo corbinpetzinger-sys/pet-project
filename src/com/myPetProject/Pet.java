189451202f4e206218e186245e528641d5f3e875
Corbin Petzinger <corbin.petzinger@gmail.com>
2025-09-24 12:06:39 +0000

    This...Is...A... Project

--- src/com/myPetProject/Pet.java
+++ src/com/myPetProject/Pet.java
@@ -10,7 +10,19 @@ this.species = species;
 
 }
 public void getSpecies() {
-
-System.out.print("");
-}
-}
+    System.out.println("deciphering inuendos");
+    try {
+        Thread.sleep(4000);
+    }
+    catch(InterruptedException e) {
+        Thread.currentThread().interrupt();
+    }
+    System.out.println("discovering animals");
+     try {
+        Thread.sleep(4000);
+    }
+    catch (InterruptedException e){
+        Thread.currentThread().interrupt();
+    }
+System.out.println("This is a... " + species);
+}}
